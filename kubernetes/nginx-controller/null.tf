be905087251ebdc358668b2e05294ed64c62d3dc
Oladele Williams <126907037+Olaalt@users.noreply.github.com>
2024-03-17 01:54:27 -0400

    Update null.tf

--- kubernetes/nginx-controller/null.tf
+++ kubernetes/nginx-controller/null.tf
@@ -1,6 +1,6 @@
-resource "null_resource" "get_nlb_hostname" {
+ast-1resource "null_resource" "get_nlb_hostname" {
     provisioner "local-exec" {
-        command = "aws eks update-kubeconfig --name hr-dev-eks-demo --region eu-west-2 && kubectl get svc load-nginx  --namespace nginx-ingress -o jsonpath='{.status.loadBalancer.ingress[*].hostname}' > ${path.module}/lb_hostname.txt"
+        command = "aws eks update-kubeconfig --name hr-dev-eks-capstone --region us-east-1 && kubectl get svc load-nginx  --namespace nginx-ingress -o jsonpath='{.status.loadBalancer.ingress[*].hostname}' > ${path.module}/lb_hostname.txt"
     }
     depends_on = [
       helm_release.ingress_nginx
