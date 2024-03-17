ab486f6441d631689dff29f8a3aa4027f8ee953a
Oladele Williams <onelovewilliams0310@gmail.com>
2024-03-17 13:38:24 -0400

    kub-provider-ch

--- kubernetes/micro-service/micro-provider.tf
+++ kubernetes/micro-service/micro-provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
+      version = ">= 2.27.0"
     }
   }
 }
@@ -16,7 +16,7 @@ data "aws_eks_cluster_auth" "hr-dev-eks-capstone_auth" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.hr-dev-eks-capstone.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.hr-dev-eks-capstone.certificate_authority[0].data)
-  
+   version     = "2.16.1"
   config_path = "~/.kube/config"
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
