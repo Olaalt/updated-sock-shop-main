aedc3be84f870e1a22b37d9afe71d164c7fc8db9
Oladele Williams <onelovewilliams0310@gmail.com>
2024-03-17 14:51:34 -0400

    move

--- kubernetes/micro-service/micro-provider.tf
+++ kubernetes/micro-service/micro-provider.tf
@@ -16,7 +16,7 @@ data "aws_eks_cluster_auth" "hr-dev-eks-capstone_auth" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.hr-dev-eks-capstone.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.hr-dev-eks-capstone.certificate_authority[0].data)
-   version     = "2.16.1"
+  
   config_path = "~/.kube/config"
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
